b72245d69788a033b2815b976628e9edb82dd052
Batiuszka Maroz <david.prajzner@op.pl>
2020-03-01 14:10:06 +0100

    Navigation built

---
+++ src/scripts/app.js
@@ -0,0 +1,6 @@
+//Bundled Sass file import
+import '../styles/main.scss';
+
+window.addEventListener('load', () => {
+  document.querySelector('.preload').classList.remove('preload');
+});
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/scripts/app.js',
+  },
+  output: {
+    filename: 'scripts/[name].js',
+    path: path.join(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        //Babel polyfill
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                { useBuiltIns: 'usage', corejs: { version: 3.6 } },
+              ],
+            ],
+          },
+        },
+      },
+      {
+        //Sass loader
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '/dist/'),
+    inline: true,
+    host: 'localhost',
+    port: 8080,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      inject: 'head',
+      chunks: ['app'],
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      defer: ['app'],
+    }),
+  ],
+};
