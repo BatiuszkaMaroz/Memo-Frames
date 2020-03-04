92d57a941ebeca5b24bbadf8b100f52861e429b5
Batiuszka Maroz <david.prajzner@op.pl>
2020-03-04 20:53:07 +0100

    Test Successfully completed :DDD

--- src/scripts/view/menu-controller.js
+++ src/scripts/view/menu-controller.js
@@ -1,10 +1,19 @@
+const backdrop = document.querySelector('.backdrop');
 const uploadButton = document.querySelector('.op--upload');
 const helpButton = document.querySelector('.op--help');
 
 uploadButton.addEventListener('click', () => {
-  document.querySelector('.backdrop--upload').style.animationName = 'open';
+  const coords = uploadButton.getBoundingClientRect();
+  backdrop.style.top = `${coords.top}px`;
+  backdrop.style.left = `${coords.left}px`;
+  backdrop.style.display = 'block';
+  backdrop.style.animationName = 'open';
 });
 
 helpButton.addEventListener('click', () => {
-  document.querySelector('.backdrop--help').style.animationName = 'open';
+  const coords = helpButton.getBoundingClientRect();
+  backdrop.style.top = `${coords.top}px`;
+  backdrop.style.left = `${coords.left}px`;
+  backdrop.style.display = 'block';
+  backdrop.style.animationName = 'open';
 });
